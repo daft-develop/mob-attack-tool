81f6b50aad6d85f29752c4f61c0b02c283e52098
daft-develop <59496570+daft-develop@users.noreply.github.com>
2025-03-15 11:39:25 -0600

    Add support for unit testing with Quench

---
+++ TESTING.md
@@ -0,0 +1,26 @@
+# TESTING
+
+## Test Setup
+
+I'm using the wonderful [Quench](https://foundryvtt.com/packages/quench) module for performing unit tests within Foundry. You can install the module like any other, enable it in your test world, and then uncomment the initQuenchTests() line in mobAttack.js to have it automatically bring in the tests.
+
+The tests perform a preflight suite to ensure your actors are setup correctly, but generally you'll need to manually set up the following:
+
+* A scene named 'Scene' that is active, global lighting enabled, token vision disabled, and no background image. This is to ensure reloading the page for repeat testing is reasonably quick
+
+* Import the starter hero "Randal" with the following changes
+  * Level him up to level 5 to get Extra Attack, and use the level 4 ASI to bump STR up to 18
+  * Drag one copy of Randal onto the scene. Probably not necessary but MAT uses tokens as it's baseline reference
+  * Add a lot of custom equipment to help test all the different attack bonus modifiers
+    * 'Longsword' - Standard equipment
+    * 'Longbow' - Standard equipment
+    * 'Handaxe (CHA)' - Change attribute to CHA
+    * 'Handaxe (No Prof)' - Change to "Not Proficient"
+    * 'Handaxe (None)' - Change attribute to 'None'
+    * 'Handaxe (Bonus ToHit)' - Add ToHit Bonus of 10
+    * 'Handaxe (Flat)' - Enable Flat Bonus, Bonus ToHit of 11
+    * 'Battleaxe +3' - SRD +3 battleaxe puts the bonus in the item description field
+    * 'Handaxe, +1' - Use "Magic Weapon" spell to at a +1 using active effects
+    * 'Handaxe, +5' - Modify "Magic Weapon" spell with the additional `activities[attack].attack.flat` override true and `activities[attack].attack.bonus` override `5`
+    * 'Fire Bolt' - Default import
+    * 'Fire Bolt (CON)' - Change casting attribute to CON
---
+++ scripts/quench.js
@@ -0,0 +1,135 @@
+import { getAttackBonus } from './utils.js'
+
+export function initQuenchTests() {
+  Hooks.on('quenchReady', (quench) => {
+    quench.registerBatch(
+      'mat.preflight',
+      (context) => {
+        const { describe, it, expect } = context
+
+        describe('Scene setup', function () {
+          const activeScene = canvas.scene
+          it('should have the correct scene', function () {
+            expect(activeScene.name).to.equal('Scene')
+            expect(activeScene.active).to.equal(true)
+          })
+          // the rest of these settings and trying to decrease load time
+          // since we're constantly refreshing while testing
+          it('should have global lighting enabled', function () {
+            expect(activeScene.environment.globalLight.enabled).to.equal(true)
+          })
+          it('should have token vision disabled', function () {
+            expect(activeScene.tokenVision).to.equal(false)
+          })
+          it('should have fog exploration disabled', function () {
+            expect(activeScene.fog.exploration).to.equal(false)
+          })
+          it('should have no background set', function () {
+            expect(activeScene.background.src).to.be.a('null')
+          })
+        }),
+        describe('Randal setup', function () {
+          const randalToken = canvas.tokens.placeables.find(t => t.name == 'Randal')
+          const randalActor = randalToken.actor
+          const randalItems = randalActor.items
+          it('should have one and only one Randal on the Canvas', function () {
+            // one and only one Randal to make .find selection repeatable
+            expect(canvas.tokens.placeables.filter(t => t.name == 'Randal')).to.have.lengthOf(1)
+          })
+          it('should have Randal at level 5', function () {
+            expect(randalActor.system.details.level).to.equal(5)
+          })
+          it('should have Extra Attack', function () {
+            expect(randalActor.items.filter(i => i.name == 'Extra Attack')).to.have.lengthOf(1)
+          })
+          it('should have Abilities set to 18/14/15/9/13/11', function () {
+            // level 4 ASI into STR
+            expect(randalActor.system.abilities.str.value).to.equal(18)
+            expect(randalActor.system.abilities.dex.value).to.equal(14)
+            expect(randalActor.system.abilities.con.value).to.equal(15)
+            expect(randalActor.system.abilities.int.value).to.equal(9)
+            expect(randalActor.system.abilities.wis.value).to.equal(13)
+            expect(randalActor.system.abilities.cha.value).to.equal(11)
+          })
+          it('should have all test equipment', function () {
+            expect(randalItems.filter(i => i.name == 'Longsword')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Longbow')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe (CHA)')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe (No Prof)')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe (None)')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe (Bonus ToHit)')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe (Flat)')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe, +1')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Handaxe, +5')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Battleaxe +3')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Fire Bolt')).to.have.lengthOf(1)
+            expect(randalItems.filter(i => i.name == 'Fire Bolt (CON)')).to.have.lengthOf(1)
+          })
+        })
+      },
+      { displayName: 'MAT: Preflight Checks' },
+    )
+    quench.registerBatch(
+      'mat.player',
+      (context) => {
+        const { describe, it, expect } = context
+
+        const randalToken = canvas.tokens.placeables.find(t => t.name == 'Randal')
+        const randalActor = randalToken.actor
+        const randalItems = randalActor.items
+
+        describe('Player Attack Bonus', function () {
+          it('should handle default prof (str for melee)', function () {
+            const longsword = randalItems.find(i => i.name == 'Longsword')
+            expect(getAttackBonus(longsword)).to.equal(7)
+          })
+          it('should handle default prof (dex for ranged)', function () {
+            const longbow = randalItems.find(i => i.name == 'Longbow')
+            expect(getAttackBonus(longbow)).to.equal(5)
+          })
+          it('should handle default spell prof (int)', function () {
+            const firebolt = randalItems.find(i => i.name == 'Fire Bolt')
+            expect(getAttackBonus(firebolt)).to.equal(2)
+          })
+          it('should handle manually set weapon prof', function () {
+            const handaxe_manual_cha = randalItems.find(i => i.name == 'Handaxe (CHA)')
+            expect(getAttackBonus(handaxe_manual_cha)).to.equal(3)
+          })
+          it('should handle manually set spell prof', function () {
+            const firebolt_con = randalItems.find(i => i.name == 'Fire Bolt (CON)')
+            expect(getAttackBonus(firebolt_con)).to.equal(5)
+          })
+          it('should handle not prof', function () {
+            const handaxe_no_prof = randalItems.find(i => i.name == 'Handaxe (No Prof)')
+            expect(getAttackBonus(handaxe_no_prof)).to.equal(4)
+          })
+          it('should handle "none" prof', function () {
+            const handaxe_attribute_none = randalItems.find(i => i.name == 'Handaxe (None)')
+            expect(getAttackBonus(handaxe_attribute_none)).to.equal(3)
+          })
+          it('should handle to hit bonus', function () {
+            const handaxe_bonus_tohit = randalItems.find(i => i.name == 'Handaxe (Bonus ToHit)')
+            expect(getAttackBonus(handaxe_bonus_tohit)).to.equal(17)
+          })
+          it('should handle a flat to hit bonus', function () {
+            const handaxe_bonus_tohit = randalItems.find(i => i.name == 'Handaxe (Flat)')
+            expect(getAttackBonus(handaxe_bonus_tohit)).to.equal(11)
+          })
+          it('should handle magic bonus in details', function () {
+            const magic_battleaxe = randalItems.find(i => i.name == 'Battleaxe +3')
+            expect(getAttackBonus(magic_battleaxe)).to.equal(10)
+          })
+          it('should handle magical enhancement bonuses', function () {
+            const enhanced_handaxe = randalItems.find(i => i.name == 'Handaxe, +1')
+            expect(getAttackBonus(enhanced_handaxe)).to.equal(8)
+          })
+          it('should handle magical enhancement flat attack modified', function () {
+            const flat_enhanced_handaxe = randalItems.find(i => i.name == 'Handaxe, +5')
+            expect(getAttackBonus(flat_enhanced_handaxe)).to.equal(5)
+          })
+        })
+      },
+      { displayName: 'MAT: Player Character Checks' },
+    )
+  })
+}
